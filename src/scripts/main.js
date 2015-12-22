e41091f41153fcabf9758a4784c74f2e25df40f8
lejoe <ciao@lejoe.com>
2015-12-22 19:29:28 +0100

    Adds the content iframe

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,23 @@
 
 // This is how you require a component from node_modules
 // Make sure to add it to the vendors in /src/scripts/vendor.js
-var mediaQuery = require('sensible/mediaQuery');
+var $ = require('jquery');
+var contentIframeEl = $('#styleguide-content');
+var navItems = $('.nav-item');
+var activeClass = 'nav-item--active';
 
 // The code for the website comes here.
-if ( mediaQuery.isNot('mobile') ){
-  console.log("I'm a not a mobile")
+navItems.each(function(i) {
+  $(this).find('.nav-link').on('click', function(e){
+    e.preventDefault();
+    changeContent($(this).attr('href'));
+    navItems.removeClass(activeClass);
+    $(this).parent().addClass(activeClass);
+  });
+})
+
+
+function changeContent(url) {
+  contentIframeEl.attr('src', url);
+  return false;
 }
-
-
