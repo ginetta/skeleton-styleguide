7e112fa635dff723b3c69ef0640e033c27476bfb
haluvibe <water7earth@gmail.com>
2016-07-06 13:57:03 +0200

    styleguide-namespace task ready for review

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -1,8 +1,6 @@
 'use strict';
 
 // Require here all the files that you want to be bundled in the vendor.js file
-<<<<<<< HEAD
+
 require('jquery');
-=======
 require('sensible/mediaQuery');
->>>>>>> 08bc9ce67eaf39d111e933173b34d3a383ab6964
