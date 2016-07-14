2179c7758d02130624ebe159cfbd719553a48e07
haluvibe <water7earth@gmail.com>
2016-07-14 21:30:51 +0200

    deeplinks wip

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,18 +7,34 @@ var contentIframeEl = $('#styleguide-content');
 var navItems = $('.js-styleguide-nav-item');
 var activeClass = 'styleguide-nav-item--active';
 
+// load the correct page in the iframe
+window.onload = function() {
+  var currentURL = window.location.href;
+  if (currentURL.includes("?page=")) {
+    var url = convertURL(window.location.href);
+    changeContent(url);
+  };
+};
+
 // The code for the website comes here.
 navItems.each(function(i) {
   $(this).find('.js-styleguide-nav-link').on('click', function(e){
+    var target = $(this).attr('href');
     e.preventDefault();
-    changeContent($(this).attr('href'));
+    changeContent(target);
+    window.history.pushState('page2', 'Title', '?page=' + target);
     navItems.removeClass(activeClass);
     $(this).parent().addClass(activeClass);
   });
 })
 
-
 function changeContent(url) {
   contentIframeEl.attr('src', url);
   return false;
+}
+
+function convertURL(url) {
+  var page = url.substr(url.indexOf("?") + 6);
+  var baseURL = window.location.href.split('?')[0];
+  return baseURL + page;
 }
